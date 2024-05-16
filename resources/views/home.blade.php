74a8688d3d3558f014a8eed8251b95069743de30
StefanRaluca <raluca.bubulina@yahoo.com>
2024-05-16 16:39:36 +0200

    add jumbotron in main section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.app')
 @section('content')
-    main
+    <div class="p-5 mb-4 bg-light rounded-3">
+        <div class="container-fluid py-5">
+            <h1 class="display-5 fw-bold">Custom jumbotron</h1>
+            <p class="col-md-8 fs-4 d-flex">
+                Using a series of utilities, you can create this jumbotron, just
+                like the one in previous versions of Bootstrap. Check out the
+                examples below for how you can remix and restyle it to your liking.
+                <img src="https://pad.mymovies.it/filmclub/2017/02/113/locandina.jpg" alt="">
+
+            </p>
+            <a href="{{ route('comics.index') }}" class="btn btn-primary">View all comics</a>
+        </div>
+    </div>
 @endsection
